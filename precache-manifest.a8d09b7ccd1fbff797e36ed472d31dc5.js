bb7ef1f67b0fc00b490a7d1f81a781205f983c9a
Jonas Manuel <jonasmanuel1@web.de>
2020-07-01 23:39:12 +0200

    Updates

---
+++ precache-manifest.a8d09b7ccd1fbff797e36ed472d31dc5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0941af8dc46305bf593ecd0a03b9a560",
+    "url": "/BoxedIn/index.html"
+  },
+  {
+    "revision": "1dc5e6085127b63ad13b",
+    "url": "/BoxedIn/static/css/main.d895bc7c.chunk.css"
+  },
+  {
+    "revision": "82998ac908d84d8aa3a4",
+    "url": "/BoxedIn/static/js/2.88fb8348.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/BoxedIn/static/js/2.88fb8348.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1dc5e6085127b63ad13b",
+    "url": "/BoxedIn/static/js/main.81e215f3.chunk.js"
+  },
+  {
+    "revision": "00375b453bebf6f8727e",
+    "url": "/BoxedIn/static/js/runtime-main.3f200c17.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/BoxedIn/static/media/logo.ee7cd8ed.svg"
+  }
+]);
